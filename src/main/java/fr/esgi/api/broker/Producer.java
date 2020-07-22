7ac714b8b5db885260697bf29090c9c6bbe5b7b9
FAHRAOUI Zakaria <zakaria.fahraoui@gmail.com>
2020-07-22 11:04:56 +0200

    add delivery mode for queue

--- src/main/java/fr/esgi/api/broker/Producer.java
+++ src/main/java/fr/esgi/api/broker/Producer.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageBuilder;
+import org.springframework.amqp.core.MessageDeliveryMode;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,10 +32,11 @@ public class Producer {
             Message message = MessageBuilder
                     .withBody(msgJson.getBytes())
                     .setContentType(MessageProperties.CONTENT_TYPE_JSON)
+                    .setDeliveryMode(MessageDeliveryMode.PERSISTENT)
                     .build();
             //this.rabbitTemplate.convertAndSend(queueName, message);
             this.rabbitTemplate.convertAndSend(exchange, routingkey, message);
-            log.info("Send Object = " + msg.toString());
+            log.info("Send Object = " + msg);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
